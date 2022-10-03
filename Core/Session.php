fc35f096f3cd0f2ee5ff9de92f24556fe8ac4b30
Sattelpunkt <keven.sattelpunkt@mail.de>
2022-10-03 17:17:26 +0200

    Session eingebaut Vorbereitung auf die Session MSG

--- Core/Session.php
+++ Core/Session.php
@@ -2,7 +2,29 @@
 
 namespace Core;
 
-class Session{
+class Session
+{
 
-    public static
+    public static function exists($name)
+    {
+        return isset($_SESSION['name']);
+    }
+
+    public static function set($name, $value)
+    {
+        $_SESSION[$name] = $value;
+    }
+
+    public static function get($name)
+    {
+        if (self::exists($name) && !empty($_SESSION[$name])) {
+            return $_SESSION[$name];
+        }
+        return false;
+    }
+
+    public static function delete($name)
+    {
+        unset($_SESSION[$name]);
+    }
 }
