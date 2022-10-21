a8fe23fcd9c6af78b3b74d1ed389a3697be239ed
Sattelpunkt <keven.sattelpunkt@mail.de>
2022-10-21 22:03:03 +0200

    test commit

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('PROOT', str_replace('public', '', __DIR__));
 define('URL',$_SERVER['HTTP_HOST']."/");
 
-
+echo PROOT;
 require_once PROOT . DS . 'Core' . DS . 'Autoload.php';
 
 require_once PROOT . DS . 'App' . DS . 'routes.php';
